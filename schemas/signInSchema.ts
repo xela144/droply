8fffaa5cf8a1a5132266a55ea1f93725c2078cb4
xela144 <alex.d.bardales@gmail.com>
2025-05-09 19:33:07 -0700

    Zod schema for signup/signin

---
+++ schemas/signInSchema.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const signInSchema = z.object({
+  identifier: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Please enter a valid email" }),
+  password: z.string().min(1, { message: "Password is required" }),
+});
---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+export const signUpSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email({ message: "Please enter a valid email" }),
+    password: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .min(10, { message: "Password should be minimum of 10 characters" })
+      .max(40, { message: "Max password length is 40 characters" }),
+    passwordConfirmation: z
+      .string()
+      .min(1, { message: "Please confirm your password" }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  });
